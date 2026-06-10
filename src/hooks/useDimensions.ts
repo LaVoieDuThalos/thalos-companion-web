6ea7f942431907147ed9d1761e992eb20a8955b4
LEBLOND <christophe.leblond@cegedim.com>
2026-06-10 15:47:20 +0200

    A tester

--- src/hooks/useDimensions.ts
+++ src/hooks/useDimensions.ts
@@ -2,17 +2,25 @@ import { useEffect, useState } from 'react';
 import { Globals } from '../constants/Globals.ts';
 
 export function useDimensions() {
+  const [width, setWidth] = useState(window.innerWidth);
   const [height, setHeight] = useState(
     window.innerHeight - Globals.HEADER_HEIGHT_IN_PX - Globals.FOOTER_HEIGHT_IN_PX
   );
 
   useEffect(() => {
     const resizeHandler = () => {
+      setWidth(window.innerWidth);
       setHeight(window.innerHeight - Globals.HEADER_HEIGHT_IN_PX - Globals.FOOTER_HEIGHT_IN_PX);
     };
+
+    resizeHandler();
     window.addEventListener('resize', resizeHandler);
-    return () => window.removeEventListener('resize', resizeHandler);
+    window.addEventListener('orientationchange', resizeHandler);
+    return () => {
+      window.removeEventListener('resize', resizeHandler);
+      window.removeEventListener('orientationchange', resizeHandler);
+    };
   }, []);
 
-  return [window.innerWidth, height]
+  return [width, height];
 }
